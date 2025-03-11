fca551f891ef8ed0fd4c9eb3f8524764ba3930e6
3dblur <hasansyed2001@gmail.com>
2025-03-11 11:48:28 +0530

    title fix

--- src/components/Sections/Landing.jsx
+++ src/components/Sections/Landing.jsx
@@ -9,9 +9,9 @@ const Landing = () => {
         <img src="/loog.png" alt="UI/OS Logo" className="logo" />
       </div>
       <div className="landing-content">
-        <h1 className="landing-title">UI Marketplace</h1>
+        <h1 className="landing-title">Find The Best UI <p> Ship The Best UX </p></h1>
         <p className="landing-subtitle">
-          Discover and purchase scalable user interfaces designed by leading UX engineers for fast deployment.
+          Discover and purchase scalable user interfaces designed by leading UX engineers with scalable AI development for fast deployment.
         </p>
         <div className="landing-buttons">
           <Button>Explore</Button>
